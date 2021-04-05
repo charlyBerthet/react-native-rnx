6195179f446775389f7e4d5e6a04c9c35105abc7
Charly Berthet <charlyberthet@pc396.home>
2021-04-05 10:51:46 +0200

    fix(ui): fix ui

--- src/localization/services/i18n.ts
+++ src/localization/services/i18n.ts
@@ -2,12 +2,12 @@ import { initReactI18next } from 'react-i18next';
 import i18next, { Resource } from 'i18next';
 import RNLocalize from 'react-native-localize';
 
-export const initLocalization = (_resources: { lng: string; file: any }[]) => {
+export const initLocalization = (_resources: { [lng: string]: any }[]) => {
   const locale = RNLocalize.getLocales()[0].languageCode.substr(0, 2);
   const resources: Resource = {};
-  for (const res of _resources) {
-    resources[res.lng as string] = {
-      common: res.file,
+  for (const resLng in _resources) {
+    resources[resLng] = {
+      common: _resources[resLng].file,
     };
   }
   i18next.use(initReactI18next).init({
