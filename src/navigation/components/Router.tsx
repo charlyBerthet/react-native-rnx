2972343cedcc8f27d4e195cce6020658a5256f93
Charly Berthet <charlyberthet@pc396.home>
2021-04-06 11:02:33 +0200

    fix(ui): fix ui

--- src/navigation/components/Router.tsx
+++ src/navigation/components/Router.tsx
@@ -49,13 +49,13 @@ export const Router = (props: Props) => {
         {tabs.length > 1 ? ( // Multiple tabs, show bottom bar
           <Tab.Navigator
             screenOptions={({ route }) => ({
-              tabBarIcon: ({ color }) => {
+              tabBarIcon: ({ color, focused }) => {
                 return (
                   <Icon
                     name={props.tabs[route.name].iconName}
                     size={21}
                     color={color}
-                    solid={false}
+                    solid={focused}
                   />
                 );
               },
