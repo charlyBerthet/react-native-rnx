8e17fe82e6ad931dc96b9e34e7b14d07bfe8534b
Charly Berthet <charlyberthet@pc396.home>
2021-04-06 15:29:03 +0200

    fix(ui): fix ui

--- src/navigation/components/Router.tsx
+++ src/navigation/components/Router.tsx
@@ -1,5 +1,10 @@
 import * as React from 'react';
-import { View, StyleSheet, useColorScheme } from 'react-native';
+import {
+  ScrollView,
+  SafeAreaView,
+  StyleSheet,
+  useColorScheme,
+} from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 import Icon from 'react-native-vector-icons/FontAwesome5';
@@ -32,62 +37,64 @@ export const Router = (props: Props) => {
   }));
 
   return (
-    <View style={[styles.root, { backgroundColor: theme.bgColor }]}>
-      <NavigationContainer
-        theme={{
-          dark: isDarkTheme,
-          colors: {
-            primary: theme.primaryColor,
-            background: theme.bgColor,
-            card: theme.bgColor,
-            text: theme.txtColor,
-            border: theme.bgColor,
-            notification: theme.txtColor,
-          },
-        }}
-      >
-        {tabs.length > 1 ? ( // Multiple tabs, show bottom bar
-          <Tab.Navigator
-            screenOptions={({ route }) => ({
-              tabBarIcon: ({ color }) => {
-                return (
-                  <Icon
-                    name={props.tabs[route.name].iconName}
-                    size={21}
-                    color={color}
-                    solid={true}
-                  />
-                );
-              },
-            })}
-            tabBarOptions={{
-              activeTintColor: theme.primaryColor,
-              inactiveTintColor: 'gray',
-            }}
-          >
-            {tabs.map((s) => (
-              <Tab.Screen
-                key={s.name}
-                name={s.name}
-                options={{
-                  title: s.title || s.name,
-                }}
-              >
-                {(stackProps) => (
-                  <Stack
-                    {...stackProps}
-                    screens={s.screens}
-                    initial={s.initial}
-                  />
-                )}
-              </Tab.Screen>
-            ))}
-          </Tab.Navigator>
-        ) : tabs.length === 1 ? ( // One tab, don't show bottom bar
-          <Stack screens={tabs[0].screens} initial={tabs[0].initial} />
-        ) : undefined}
-      </NavigationContainer>
-    </View>
+    <ScrollView style={[styles.root, { backgroundColor: theme.bgColor }]}>
+      <SafeAreaView>
+        <NavigationContainer
+          theme={{
+            dark: isDarkTheme,
+            colors: {
+              primary: theme.primaryColor,
+              background: theme.bgColor,
+              card: theme.bgColor,
+              text: theme.txtColor,
+              border: theme.bgColor,
+              notification: theme.txtColor,
+            },
+          }}
+        >
+          {tabs.length > 1 ? ( // Multiple tabs, show bottom bar
+            <Tab.Navigator
+              screenOptions={({ route }) => ({
+                tabBarIcon: ({ color }) => {
+                  return (
+                    <Icon
+                      name={props.tabs[route.name].iconName}
+                      size={21}
+                      color={color}
+                      solid={true}
+                    />
+                  );
+                },
+              })}
+              tabBarOptions={{
+                activeTintColor: theme.primaryColor,
+                inactiveTintColor: 'gray',
+              }}
+            >
+              {tabs.map((s) => (
+                <Tab.Screen
+                  key={s.name}
+                  name={s.name}
+                  options={{
+                    title: s.title || s.name,
+                  }}
+                >
+                  {(stackProps) => (
+                    <Stack
+                      {...stackProps}
+                      screens={s.screens}
+                      initial={s.initial}
+                    />
+                  )}
+                </Tab.Screen>
+              ))}
+            </Tab.Navigator>
+          ) : tabs.length === 1 ? ( // One tab, don't show bottom bar
+            <Stack screens={tabs[0].screens} initial={tabs[0].initial} />
+          ) : undefined}
+        </NavigationContainer>
+      </SafeAreaView>
+    </ScrollView>
   );
 };
 
