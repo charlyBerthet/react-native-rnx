40fe15cc1d8aeb2fcfe1f1c1e08dca591cf62db7
Charly Berthet <charlyberthet@pc396.home>
2021-04-06 16:04:05 +0200

    fix(ui): fix ui

--- src/ui/components/ScrollableScreen.tsx
+++ src/ui/components/ScrollableScreen.tsx
@@ -1,4 +1,5 @@
-import React from 'react';
+import React, { useLayoutEffect } from 'react';
+import { useNavigation } from '@react-navigation/native';
 import { SafeAreaView, ScrollView, StyleSheet } from 'react-native';
 
 import { Title } from './Title';
@@ -11,8 +12,19 @@ interface Props {
 }
 
 export const ScrollableScreen = (props: Props) => {
+  const navigation = useNavigation();
+
+  useLayoutEffect(() => {
+    navigation.setOptions({
+      title: props.title,
+    });
+  }, [props.title, navigation]);
+
   return (
-    <ScrollView>
+    <ScrollView
+      onScrollToTop={() => console.log('onScrollToTop')}
+      onScroll={() => console.log('onScroll')}
+    >
       <SafeAreaView style={styles.root}>
         <Title>{props.title}</Title>
         <Subtitle>{props.subtitle}</Subtitle>
