3fb25711e9ade93bb00eb6bd0a7016506743f197
Charly Berthet <charlyberthet@pc396.home>
2021-04-07 17:47:15 +0200

    fix(ui): fix ui

--- src/theme/constants/darkTheme.ts
+++ src/theme/constants/darkTheme.ts
@@ -3,7 +3,8 @@ import type Theme from '../models/Theme';
 const darkTheme: Theme = {
   bgColor: '#000',
   txtColor: '#fff',
-  primaryColor: '#055640',
+  primaryTxtColor: '#055640',
+  primaryColor: '#0D8060',
   secondaryColor: '#EA7575',
   thirdColor: '#0694FF',
   txtColorOnPrimaryColor: '#fff',
--- src/theme/models/Theme.ts
+++ src/theme/models/Theme.ts
@@ -2,6 +2,7 @@ export default interface Theme {
   bgColor: string;
   txtColor: string;
   primaryColor: string;
+  primaryTxtColor: string;
   secondaryColor: string;
   thirdColor: string;
   txtColorOnPrimaryColor: string;
