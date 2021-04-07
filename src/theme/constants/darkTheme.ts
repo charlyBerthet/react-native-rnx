ae001d38a2ff7e8a1e1e987578da745495dee1d3
Charly Berthet <charlyberthet@pc396.home>
2021-04-07 17:45:14 +0200

    fix(ui): fix ui

--- src/theme/constants/darkTheme.ts
+++ src/theme/constants/darkTheme.ts
@@ -3,7 +3,7 @@ import type Theme from '../models/Theme';
 const darkTheme: Theme = {
   bgColor: '#000',
   txtColor: '#fff',
-  primaryColor: '#0D8060',
+  primaryColor: '#055640',
   secondaryColor: '#EA7575',
   thirdColor: '#0694FF',
   txtColorOnPrimaryColor: '#fff',
--- src/theme/constants/lightTheme.ts
+++ src/theme/constants/lightTheme.ts
@@ -3,7 +3,7 @@ import type Theme from '../models/Theme';
 const lightTheme: Theme = {
   bgColor: '#fff',
   txtColor: '#444',
-  primaryColor: '#0D8060',
+  primaryColor: '#055640',
   secondaryColor: '#EA7575',
   thirdColor: '#0694FF',
   txtColorOnPrimaryColor: '#fff',
