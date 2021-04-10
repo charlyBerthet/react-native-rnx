87650c5fe59fa06457d7fdfa97412ed870c5573e
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-04-10 14:36:06 +0200

    fix(ui): fix ui

--- src/ui/components/Countdown.tsx
+++ src/ui/components/Countdown.tsx
@@ -1,6 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import { View, StyleSheet, Dimensions } from 'react-native';
-import Svg, { Circle } from 'react-native-svg';
+import Svg, { Circle, Path } from 'react-native-svg';
 import { useTheme } from '../../theme';
 import type CommonViewProps from '../models/CommonViewProps';
 import { Text } from './Text';
@@ -14,6 +14,26 @@ interface Props extends CommonViewProps {
 }
 
 const SIZE = Math.min(Dimensions.get('window').width - 40, 250);
+
+function generateSvgArc(percentage: number, radius: number) {
+  if (percentage === 100) percentage = 99.999;
+  const a = (percentage * 2 * Math.PI) / 100; // angle (in radian) depends on percentage
+  const r = radius; // radius of the circle
+  var rx = r,
+    ry = r,
+    xAxisRotation = 0,
+    largeArcFlag = 1,
+    sweepFlag = 1,
+    x = r + r * Math.sin(a),
+    y = r - r * Math.cos(a);
+  if (percentage <= 50) {
+    largeArcFlag = 0;
+  } else {
+    largeArcFlag = 1;
+  }
+
+  return `A${rx} ${ry} ${xAxisRotation} ${largeArcFlag} ${sweepFlag} ${x} ${y}`;
+}
 
 export const Countdown = (props: Props) => {
   const [now, setNow] = useState(0);
@@ -32,16 +52,25 @@ export const Countdown = (props: Props) => {
     ((now - startsAt) / (endsAt - startsAt)) * 100
   );
 
+  const circleRadius = 45;
+
   return (
     <View style={[styles.root, props.style]}>
       <Svg height="100%" width="100%" viewBox="0 0 100 100" style={styles.svg}>
         <Circle
           cx="50"
           cy="50"
-          r="45"
+          r={circleRadius}
           stroke={theme.primaryColor}
           strokeWidth="10"
           opacity="0.15"
+        />
+        <Path
+          d={`M${circleRadius} ${circleRadius} L${circleRadius} 0 ${generateSvgArc(
+            timeSpentPercentage,
+            circleRadius
+          )} Z`}
+          fill={theme.primaryColor}
         />
       </Svg>
       <View style={[styles.txtContainer]}>
