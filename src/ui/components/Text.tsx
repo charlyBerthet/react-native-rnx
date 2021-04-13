51ba89a153f6cdd1824b5270750cc33b368678ff
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-04-13 09:31:47 +0200

    fix(ui): fix ui

--- src/ui/components/Text.tsx
+++ src/ui/components/Text.tsx
@@ -2,19 +2,23 @@ import * as React from 'react';
 import { Text as RNText, StyleSheet } from 'react-native';
 import type CommonTextProps from '../models/CommonTextProps';
 import useTheme from '../../theme/hooks/useTheme';
+import useMainColors from '../../theme/hooks/useMainColors';
 
 interface Props extends CommonTextProps {
   children: string | string[];
   primary?: boolean;
+  secondary?: boolean;
 }
 
 export const Text = (props: Props) => {
   const theme = useTheme();
+  const mainColors = useMainColors(props.secondary);
+  const shouldUseMain = props.primary || props.secondary;
   return (
     <RNText
       style={[
         styles.root,
-        { color: props.primary ? theme.primaryTxtColor : theme.txtColor },
+        { color: shouldUseMain ? mainColors.txt : theme.txtColor },
         props.style,
       ]}
     >
