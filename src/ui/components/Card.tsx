09adb0903e77def30171705e5469d755682e6264
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-04-15 14:45:59 +0200

    fix(ui): fix ui

--- src/ui/components/Card.tsx
+++ src/ui/components/Card.tsx
@@ -75,7 +75,7 @@ const styles = StyleSheet.create({
     shadowOpacity: 0.15,
     shadowRadius: 3.84,
     elevation: 5,
-    marginVertical: 15,
+    marginVertical: 10,
     flexDirection: 'row',
     alignItems: 'flex-start',
   },
