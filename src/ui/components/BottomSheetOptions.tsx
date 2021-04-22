b2b91c1741f6fdc05dd87a0792b840857d73dcd6
Charly Berthet <charlyberthet@pc396.home>
2021-04-22 10:17:00 +0200

    fix(ui): fix ui

---
+++ src/ui/components/BottomSheetOptions.tsx
@@ -0,0 +1,51 @@
+import React, { useCallback, useEffect, useRef } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Text } from './Text';
+import BottomSheet from 'reanimated-bottom-sheet';
+import type CommonViewProps from '../models/CommonViewProps';
+
+export interface BottomSheetOptionsActionsRef {
+  show: () => void;
+  hide: () => void;
+}
+
+interface Props extends CommonViewProps {
+  actionsref: (actionsRef: BottomSheetOptionsActionsRef) => void;
+}
+
+export const BottomSheetOptions = (props: Props) => {
+  const sheetRef = useRef<BottomSheet>(null);
+  const { actionsref } = props;
+
+  const show = useCallback(() => {
+    sheetRef?.current?.snapTo(450);
+  }, []);
+
+  const hide = useCallback(() => {
+    sheetRef?.current?.snapTo(0);
+  }, []);
+
+  useEffect(() => {
+    actionsref({ show, hide });
+  }, [actionsref, hide, show]);
+
+  return (
+    <BottomSheet
+      ref={sheetRef}
+      snapPoints={[450, 300, 0]}
+      borderRadius={10}
+      renderContent={() => (
+        <View style={[styles.root, props.style]}>
+          <Text>TEST</Text>
+        </View>
+      )}
+    />
+  );
+};
+
+const styles = StyleSheet.create({
+  root: {
+    paddingVertical: 7,
+    paddingHorizontal: 10,
+  },
+});
