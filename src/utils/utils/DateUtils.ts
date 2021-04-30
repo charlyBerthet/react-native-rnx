bb599911e5d91d4b4db2feb7612400ff14d75ad9
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-04-30 14:06:20 +0200

    fix(ui): fix ui

--- src/utils/utils/DateUtils.ts
+++ src/utils/utils/DateUtils.ts
@@ -52,3 +52,12 @@ export const isSameDay = (a: number, b: number) => {
     dA.getFullYear() === dB.getFullYear()
   );
 };
+
+export const resetTime = (timestamp: number) => {
+  const date = new Date(timestamp);
+  date.setHours(0);
+  date.setMinutes(0);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+  return date.getTime();
+};
