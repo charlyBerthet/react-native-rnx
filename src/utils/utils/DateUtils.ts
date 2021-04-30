7eaaa83346944cbbc87e6d84dff6f74a6db42c7b
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-04-30 15:26:15 +0200

    fix(ui): fix ui

--- src/utils/utils/DateUtils.ts
+++ src/utils/utils/DateUtils.ts
@@ -39,9 +39,11 @@ export const daysDiff = (
 };
 export const hoursToDays = (hours: number) => Math.round(hours / 24);
 export const daysToHours = (days: number) => Math.round(days * 24);
-export const msToDays = (ms: number, rounded = true) => {
+export const msToDays = (ms: number, rounded = true, decimal = 0) => {
   const res = ms / 1000 / 60 / 60 / 24;
-  return rounded ? Math.round(res) : Math.round(res * 100) * 0.01;
+  return rounded
+    ? Math.round(res * Math.pow(10, decimal)) / Math.pow(10, decimal)
+    : res;
 };
 export const msToHours = (ms: number) => Math.round(ms / 1000 / 60 / 60);
 export const hoursToMs = (hours: number) => hours * 60 * 60 * 1000;
