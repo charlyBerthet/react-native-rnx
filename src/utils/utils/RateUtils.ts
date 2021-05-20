4f909a24cdbf3a1ba0c62fcd85aef8f1fe15608f
Charly Berthet <charlyberthet@Charlys-MacBook-Pro.local>
2021-05-20 09:26:58 +0200

    fix(ui): fix ui

---
+++ src/utils/utils/RateUtils.ts
@@ -0,0 +1,61 @@
+import { Alert } from 'react-native';
+import Rate from 'react-native-rate';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
+export const askForUserFeedback = async (
+  appleAppId: string,
+  translate: (key: string) => string
+) => {
+  const openCount = parseInt(
+    (await AsyncStorage.getItem('openCount')) || '0',
+    10
+  );
+  AsyncStorage.setItem('openCount', openCount + 1 + '');
+  if (
+    openCount === 1 ||
+    openCount === 30 ||
+    openCount === 90 ||
+    openCount === 200 ||
+    openCount === 400
+  ) {
+    setTimeout(() => {
+      Alert.alert(
+        translate('rate.feelingTitle'),
+        translate('rate.feelingSubtitle'),
+        [
+          {
+            text: translate('global.no'),
+          },
+          {
+            text: translate('global.yes'),
+            onPress: () => {
+              Alert.alert(
+                translate('rate.askRateTitle'),
+                translate('rate.askRateSubtitle'),
+                [
+                  {
+                    text: translate('global.cancel'),
+                  },
+                  {
+                    text: translate('global.letsgo'),
+                    onPress: () => {
+                      Rate.rate(
+                        {
+                          AppleAppID: appleAppId,
+                          preferInApp: false,
+                        },
+                        () => {
+                          /* no opt */
+                        }
+                      );
+                    },
+                  },
+                ]
+              );
+            },
+          },
+        ]
+      );
+    }, 2000);
+  }
+};
