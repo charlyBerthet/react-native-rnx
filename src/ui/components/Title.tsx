cac0fc58368020f899ea113c9cc956692d8da451
Charly Berthet <charlyberthet94@gmail.com>
2021-11-18 16:33:44 +0100

    fix(ui): fix ui

--- src/ui/components/Title.tsx
+++ src/ui/components/Title.tsx
@@ -5,12 +5,19 @@ import useTheme from '../../theme/hooks/useTheme';
 
 interface Props extends CommonTextProps {
   children: string | string[];
+  textAlign?: 'center' | 'left' | 'right';
 }
 
 export const Title = (props: Props) => {
   const theme = useTheme();
   return (
-    <Text style={[styles.root, { color: theme.txtColor }, props.style]}>
+    <Text
+      style={[
+        styles.root,
+        { color: theme.txtColor, textAlign: props.textAlign || 'center' },
+        props.style,
+      ]}
+    >
       {props.children}
     </Text>
   );
@@ -19,7 +26,6 @@ export const Title = (props: Props) => {
 const styles = StyleSheet.create({
   root: {
     fontSize: 27,
-    textAlign: 'center',
     fontWeight: '700',
     marginHorizontal: 20,
   },
