b8e82d7251fac32d5c30b8b302b867268fda9fd7
Charly Berthet <charlyberthet94@gmail.com>
2021-11-29 09:41:36 +0100

    fix(ui): fix ui

--- src/ui/components/Row.tsx
+++ src/ui/components/Row.tsx
@@ -1,16 +1,21 @@
 import React from 'react';
-import { View, StyleSheet, TouchableOpacity } from 'react-native';
+import { View, StyleSheet, TouchableOpacity, Switch } from 'react-native';
 import { Text } from './Text';
 import { useTheme } from '../../theme';
 import type CommonViewProps from '../models/CommonViewProps';
+import Icon from 'react-native-vector-icons/FontAwesome5';
 
 export interface RowModel {
   title: string;
-  subtitle: string;
-  rightLabel: string;
-  rightLabelCaption: string;
+  subtitle?: string;
+  rightLabel?: string;
+  rightLabelCaption?: string;
   onPress?: () => void;
   onLongPress?: () => void;
+  icon?: string;
+  isEnabled?: boolean;
+  onEnabledChange?: (isEnabled: boolean) => void;
+  rightArrow?: boolean;
 }
 
 interface Props extends CommonViewProps, RowModel {
@@ -38,15 +43,49 @@ export const Row = (props: Props) => {
         onLongPress={props.onLongPress}
         activeOpacity={props.onPress ? 0.8 : 1}
       >
+        {!!props.icon && (
+          <Icon
+            name={props.icon}
+            size={18}
+            color={theme.txtColor}
+            solid={true}
+            style={styles.leftIcon}
+          />
+        )}
         <View>
           <Text style={styles.title}>{props.title}</Text>
-          <Text style={styles.subtitle}>{props.subtitle}</Text>
+          {!!props.subtitle && (
+            <Text style={styles.subtitle}>{props.subtitle}</Text>
+          )}
         </View>
         <View style={styles.rightContainer}>
-          <Text style={styles.rightLabel}>{props.rightLabel}</Text>
-          <Text style={styles.rightLabelCaption}>
-            {props.rightLabelCaption}
-          </Text>
+          {!!props.rightLabel && (
+            <Text style={styles.rightLabel}>{props.rightLabel}</Text>
+          )}
+          {!!props.rightLabelCaption && (
+            <Text style={styles.rightLabelCaption}>
+              {props.rightLabelCaption}
+            </Text>
+          )}
+          {props.isEnabled !== undefined && (
+            <Switch
+              trackColor={{ false: 'gray', true: theme.primaryColor }}
+              thumbColor={'white'}
+              onValueChange={props.onEnabledChange}
+              value={props.isEnabled}
+              style={styles.switch}
+            />
+          )}
+          {props.rightArrow && (
+            <View style={styles.rightArrow}>
+              <Icon
+                name="chevron-right"
+                size={15}
+                color={theme.txtColor}
+                solid={true}
+              />
+            </View>
+          )}
         </View>
       </TouchableOpacity>
     </View>
@@ -89,5 +128,16 @@ const styles = StyleSheet.create({
   },
   rightContainer: {
     alignItems: 'flex-end',
+  },
+  leftIcon: {
+    opacity: 0.75,
+    marginRight: 10,
+  },
+  switch: {
+    marginLeft: 10,
+  },
+  rightArrow: {
+    marginLeft: 10,
+    opacity: 0.75,
   },
 });
