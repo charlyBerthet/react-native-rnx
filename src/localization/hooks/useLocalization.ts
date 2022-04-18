0a6247989ce128315f5a2b01f09084924db1daa6
Charly Berthet <charlyberthet94@gmail.com>
2022-04-18 09:34:38 +0200

    fix(ui): fix ui

--- src/localization/hooks/useLocalization.ts
+++ src/localization/hooks/useLocalization.ts
@@ -1,10 +1,18 @@
 import { useTranslation } from 'react-i18next';
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { useCallback } from 'react';
 
 export const useLocalization = () => {
   const { t, i18n } = useTranslation('common');
 
   const localize = t;
-  const setLanguage = i18n.changeLanguage;
+  const setLanguage = useCallback(
+    async (lang: string) => {
+      i18n.changeLanguage(lang);
+      await AsyncStorage.setItem('rnx:lang', lang);
+    },
+    [i18n]
+  );
 
   return { localize, setLanguage };
 };
--- src/localization/services/i18n.ts
+++ src/localization/services/i18n.ts
@@ -1,9 +1,16 @@
 import { initReactI18next } from 'react-i18next';
 import i18next, { Resource } from 'i18next';
 import RNLocalize from 'react-native-localize';
+import AsyncStorage from '@react-native-async-storage/async-storage';
 
-export const initLocalization = (_resources: { [lng: string]: any }) => {
-  const locale = RNLocalize.getLocales()[0].languageCode.substr(0, 2);
+export const initLocalization = async (_resources: { [lng: string]: any }) => {
+  let locale = RNLocalize.getLocales()[0].languageCode.substr(0, 2);
+
+  const savedLang = await AsyncStorage.getItem('rnx:lang');
+  if (savedLang && Object.keys(_resources).includes(savedLang)) {
+    locale = savedLang;
+  }
+
   const resources: Resource = {};
   for (const resLng in _resources) {
     resources[resLng] = {
@@ -11,7 +18,7 @@ export const initLocalization = (_resources: { [lng: string]: any }) => {
     };
   }
 
-  i18next.use(initReactI18next).init({
+  await i18next.use(initReactI18next).init({
     interpolation: { escapeValue: false },
     lng: locale,
     fallbackLng: 'en',
