a654b4a223abb6f94ebf8c0f20ce671b8d9bdb63
Charly Berthet <charlyberthet94@gmail.com>
2024-09-16 11:53:10 +0200

    fix(some): fix some bugs

--- src/navigation/components/ExtraBottomView.tsx
+++ src/navigation/components/ExtraBottomView.tsx
@@ -1,8 +1,32 @@
+import * as React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { useSafeAreaInsets } from 'react-native-safe-area-context';
+
 interface Props {
   children?: JSX.Element;
   hiddenForScreenNames?: string[];
 }
 
 export function ExtraBottomView({ children }: Props) {
-  return children || null;
+  const { bottom: safeAreaBottomInset } = useSafeAreaInsets();
+
+  if (!children) {
+    return null;
+  }
+  return (
+    <View
+      style={[
+        styles.root,
+        {
+          marginTop: -safeAreaBottomInset,
+        },
+      ]}
+    >
+      children
+    </View>
+  );
 }
+
+const styles = StyleSheet.create({
+  root: {},
+});
