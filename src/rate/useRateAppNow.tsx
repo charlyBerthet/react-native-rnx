b576eab8cc4cc0cdaee16f0ce6b85fa24dc396a0
Charly Berthet <charlyberthet94@gmail.com>
2025-02-26 10:37:56 +0100

    fix: rate app now

--- src/rate/useRateAppNow.tsx
+++ src/rate/useRateAppNow.tsx
@@ -1,21 +1,37 @@
 import Rate from 'react-native-rate';
 import { useCallback } from 'react';
 import { useHasRatedTheApp } from './useHasRatedTheApp';
+import { Linking, Platform } from 'react-native';
 
 export const useRateAppNow = () => {
   const { setHasRatedTheApp } = useHasRatedTheApp();
   const rateAppNow = useCallback(
     (appleAppId: string, googlePackageName?: string) => {
-      Rate.rate(
-        {
-          AppleAppID: appleAppId,
-          GooglePackageName: googlePackageName,
-          preferInApp: false,
-        },
-        () => {
-          setTimeout(() => setHasRatedTheApp(true), 10000);
+      switch (Platform.OS) {
+        case 'ios': {
+          Linking.openURL(
+            `itms-apps://itunes.apple.com/app/viewContentsUserReviews/id${appleAppId}?action=write-review`
+          );
+          break;
         }
-      );
+        case 'macos': {
+          Linking.openURL(
+            `https://apps.apple.com/app/apple-store/id${appleAppId}?action=write-review`
+          );
+          break;
+        }
+        default: {
+          Rate.rate(
+            {
+              AppleAppID: appleAppId,
+              GooglePackageName: googlePackageName,
+              preferInApp: false,
+            },
+            () => {}
+          );
+        }
+      }
+      setTimeout(() => setHasRatedTheApp(true), 10000);
     },
     [setHasRatedTheApp]
   );
