6b49785916951ec16e2753d05267e2d14c61e7ed
Charly Berthet <charlyberthet94@gmail.com>
2025-04-15 17:48:11 +0200

    improve paywall

--- src/iap/UpgradeView.tsx
+++ src/iap/UpgradeView.tsx
@@ -461,7 +461,7 @@ const styles = StyleSheet.create({
     shadowRadius: 5,
     color: 'white',
     fontWeight: '900',
-    fontSize: 22,
+    fontSize: 26,
     marginBottom: 7,
   },
   featuresTxt: {
