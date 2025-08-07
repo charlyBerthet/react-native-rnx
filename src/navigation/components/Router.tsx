ab67420b6ae83f1f349dc4ba8c3af726919145b3
Charly Berthet <charlyberthet94@gmail.com>
2025-08-07 11:28:28 +0200

    fix: tab label

--- src/navigation/components/Router.tsx
+++ src/navigation/components/Router.tsx
@@ -186,7 +186,7 @@ const styles = StyleSheet.create({
   tabBarLabelStyle: {
     fontSize: 11,
     fontWeight: '500',
-    marginTop: -8,
+    marginTop: -4,
   },
   notFocusedIcon: {
     opacity: 0.7,
