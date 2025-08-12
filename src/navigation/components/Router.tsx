b5003d4fa45cb46e963e0289349bc1154f56dfc6
Charly Berthet <charlyberthet94@gmail.com>
2025-08-12 09:16:56 +0200

    fix: ipad bottom bar

--- src/navigation/components/Router.tsx
+++ src/navigation/components/Router.tsx
@@ -186,7 +186,7 @@ const styles = StyleSheet.create({
   tabBarLabelStyle: {
     fontSize: 11,
     fontWeight: '500',
-    marginTop: -4,
+    marginTop: 0,
   },
   notFocusedIcon: {
     opacity: 0.7,
